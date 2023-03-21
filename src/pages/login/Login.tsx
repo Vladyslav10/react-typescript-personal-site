4a5ee5b3a477e2659b6f20da8656040062b955b5
Vladyslav10 <vshapovalov02@gmail.com>
2023-03-21 18:46:19 +0200

    add adaptive content to main page and login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,26 @@
 import React, { FC } from 'react';
+import './login.css'
 
 const Login:FC = () => {  
 
     return (
-        <main>
-            Login
+        <main className='main'>
+            <div className='main__container container'>
+                <h1 className='main__title'>Вхід в обліковий запис</h1>
+                <form className='main__form form'>
+                    <div className="form__input">
+                        <span className='form__input_hint'>Incorrect login</span>
+                        <input className='form__login' type="text" placeholder='Введіть ваш логін' />
+                    </div>
+                    <div className="form__input">
+                        <span className='form__input_hint'>Incorrect password</span>
+                        <input className='form__password' type="password" placeholder='Введіть ваш пароль' />
+                    </div>
+                    <div className='form__button'>
+                        <button className="form__btn">Ввійти</button>
+                    </div>
+                </form>
+            </div>
         </main>
     );
 };
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,35 @@
 import React, { FC } from 'react';
+import './main.css';
 
 const Main:FC = () => {  
 
     return (
-        <main>
-            Main
+        <main className='main'>
+            <div className="main__container container">
+                <h1 className='main__title'>Main page</h1>
+                <p className='main__description'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A pariatur omnis error fuga, quos dolore quasi dignissimos dolorum corporis exercitationem. Quod iste modi ipsa odio accusamus temporibus dolorem nihil nostrum.
+                    Ratione tempora laborum voluptatem! Ad voluptas pariatur voluptate porro ut aliquam explicabo possimus earum. Iure a explicabo provident assumenda exercitationem laborum, iusto aut saepe maiores facilis officia, sapiente, animi at!
+                    Quo iure corrupti quisquam veritatis ab quia minima aliquid quidem architecto fugit neque consequatur quos iste vitae nihil beatae excepturi, ipsa non nam. Ea tempora at, quia explicabo laboriosam voluptatibus.
+                    Nemo architecto sit dolorum voluptate cupiditate hic. Porro officia tempora nulla eveniet. Nostrum quis ipsa vel recusandae quibusdam libero ea exercitationem possimus esse sed. Nemo, adipisci! Corporis, distinctio pariatur. Quidem.
+                    Consectetur perspiciatis sint nobis ad ipsa deserunt exercitationem pariatur adipisci. Fugit, deserunt similique quo ad iste consequatur impedit dolorem suscipit possimus pariatur dolorum sed beatae porro? Eum ratione odio asperiores?
+                    Illo praesentium, pariatur ut labore ipsam id harum nisi veritatis impedit ea et facere enim voluptates commodi ipsum, laudantium architecto! Sunt iste veniam ratione labore harum provident odit ullam quas.
+                    Numquam iure culpa reiciendis aspernatur. Ipsa repellendus perferendis iusto beatae possimus quidem ipsum earum non, nulla aspernatur velit culpa iure nemo atque accusantium fuga dignissimos, quod magni animi, vero soluta.
+                    Quam atque consequatur sit error voluptates unde adipisci exercitationem magni, assumenda facilis illo! Temporibus assumenda dicta exercitationem neque minus accusamus? Vero, quae! Cum cumque vel quos sequi aut velit ipsa.
+                    Excepturi iusto perferendis corrupti, dicta odit saepe earum nesciunt fugit distinctio doloremque eos amet maxime blanditiis facere! Voluptatibus quibusdam deleniti molestiae ipsam unde architecto culpa explicabo cupiditate, est saepe delectus!
+                    Saepe consequatur dolores fugit repudiandae reprehenderit. Voluptate, explicabo, dicta in sint corrupti maxime harum commodi suscipit at excepturi facilis hic culpa aliquam veniam eos? Ipsum unde reprehenderit dignissimos quaerat voluptatum.
+                    Quae sit ipsum dolorem sed suscipit mollitia quidem iste praesentium porro nihil eveniet unde, nisi ea nobis distinctio ex? Laborum, maxime perferendis. Fuga officiis repellat dolore molestiae doloribus repudiandae porro!
+                    Ab sunt ea eum saepe possimus sapiente nemo amet excepturi unde. Neque ipsa tempora repellendus perferendis porro, perspiciatis, voluptas molestiae aut vel eligendi et itaque fugiat? Voluptate reprehenderit aliquam porro.
+                    Sint perferendis a culpa, aliquam aut expedita labore officia maxime veniam distinctio eligendi cum iste deleniti exercitationem quo dolorem. Numquam recusandae voluptatum magnam dicta vel iste ut perspiciatis quae sequi?
+                    Repellat officiis vero praesentium esse odit laborum ipsam animi est temporibus iste dicta aut, officia nulla minus laboriosam ex nisi corrupti cumque. Est illum voluptate adipisci vitae cumque eius labore!
+                    Aliquid doloribus fugiat, minus praesentium temporibus fugit vero rerum, animi quam similique, cum cumque. Inventore quas exercitationem maxime laborum doloremque odit facere, asperiores beatae fuga. Reprehenderit excepturi reiciendis alias aliquam.
+                    Nesciunt culpa quas sapiente recusandae accusamus sunt velit aspernatur fugit assumenda eum! Odit molestias temporibus rerum laudantium consequatur placeat sunt. Corporis saepe aliquam quod officiis eveniet iure et repudiandae quo!
+                    Repellendus voluptates tenetur nemo reiciendis repudiandae dolores, nostrum praesentium architecto. Tempora, aspernatur adipisci alias quod, est sit nesciunt laudantium odio doloribus labore pariatur distinctio cum quidem optio repellendus voluptate animi?
+                    Ex ratione provident nobis fugit eveniet mollitia officiis doloribus rem debitis. Voluptatum deleniti iste consectetur sit quos vitae exercitationem blanditiis? Laborum illo voluptatem aut necessitatibus facilis eum in totam pariatur.
+                    Fugit cumque magni dolores, illo earum doloremque adipisci molestiae, vel alias eos accusantium libero, culpa at inventore? Voluptatum ut temporibus itaque pariatur. Eos necessitatibus tenetur distinctio corporis laborum odit consequuntur.
+                    Aut voluptatibus tenetur delectus illum placeat atque minus eum modi consequatur officia, esse suscipit, quod dolorem alias laborum corporis assumenda pariatur consectetur doloribus cumque labore blanditiis, ullam rerum? Beatae, soluta?
+                </p>
+            </div>
         </main>
     );
 };
